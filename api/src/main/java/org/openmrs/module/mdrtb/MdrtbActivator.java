955075b73756751b51d55f48a5c72caddc344d05
seekme_94 <owais.a.hussain@gmail.com>
2026-06-21 23:05:22 +0500

    Update MdrtbActivator.java
    - Merging Lab activations into MdrtbActivator

--- api/src/main/java/org/openmrs/module/mdrtb/MdrtbActivator.java
+++ api/src/main/java/org/openmrs/module/mdrtb/MdrtbActivator.java
@@ -10,9 +10,18 @@
 
 package org.openmrs.module.mdrtb;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.GlobalProperty;
+import org.openmrs.api.AdministrationService;
+import org.openmrs.api.ConceptService;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.BaseModuleActivator;
+import org.openmrs.util.OpenmrsConstants;
+import org.openmrs.util.OpenmrsUtil;
+
+import java.io.File;
 import java.util.*;
 
 /**
@@ -23,6 +32,35 @@ public class MdrtbActivator extends BaseModuleActivator {
 	private static final String DEFAULT = "default";
 	
 	private Log log = LogFactory.getLog(this.getClass());
+	
+	public static final String SPECIMEN_TYPE_CONCEPT_UUID = "commonlabtest.specimenTypeConceptUuid";
+	
+	public static final String SPECIMEN_SITE_CONCEPT_UUID = "commonlabtest.specimenSiteConceptUuid";
+	
+	public static final String TEST_UNITS_CONCEPT_UUID = "commonlabtest.testunitsConceptUuid";
+	
+	public static final String UPLOAD_FILE_DIRECTORY = "commonlabtest.fileDirectory";
+	
+	public static final String UPLOAD_FILE_EXTENSIONS = "commonlabtest.fileExtensions";
+	
+	public static final String FILE_EXTENSIONS_NAMES = ".bmp ,.jpg ,.jpeg,.jfif,.GIF,.png,.bat,.BPG,.FLV,.AVI,.MOV,.M4P,.MPG,.WMV,.3gp,.RM,.SWF,.3GP,.ACT,.AIFF,.MP3,.WAV,.OGG,.FLAC,.AU,.RAW,.docx,.docm,.dotx,.docb,.dotm,.pdf";
+	
+	public static final String LAB_ORDER_TYPE_UUID = "commonlabtest.labOrderTypeUuid";
+	
+	ConceptService conceptService;
+	
+	File dir = OpenmrsUtil.getDirectoryInApplicationDataDirectory(Context.getAdministrationService().getGlobalProperty(
+	    OpenmrsConstants.GLOBAL_PROPERTY_COMPLEX_OBS_DIR));
+	
+	private void setGlobalProperty(AdministrationService service, String prop, String val, String desc) {
+		GlobalProperty gp = service.getGlobalPropertyObject(prop);
+		if (gp == null) {
+			service.saveGlobalProperty(new GlobalProperty(prop, val, desc));
+		} else if (StringUtils.isEmpty(gp.getPropertyValue())) {
+			gp.setPropertyValue(val);
+			service.saveGlobalProperty(gp);
+		}
+	}
 	
 	/**
 	 * @see #started()
@@ -42,5 +80,32 @@ public class MdrtbActivator extends BaseModuleActivator {
 	
 	@Override
 	public void contextRefreshed() {
+		log.info("========================== Lab Test Lab contextRefreshed called ======");
+		conceptService = Context.getConceptService();
+		
+		File path = new File(dir.getPath() + "/labTestFiles");
+		
+		if (!(path.exists() && path.isDirectory())) {
+			try {
+				path.mkdir();
+			}
+			catch (Exception e) {
+				log.error(e.getMessage());
+			}
+		}
+		AdministrationService administrationService = Context.getAdministrationService();
+		setGlobalProperty(administrationService, UPLOAD_FILE_DIRECTORY, path.toString(), "");
+		setGlobalProperty(administrationService, SPECIMEN_TYPE_CONCEPT_UUID, "162476AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+		    "The UUID of a concept representing a group or set of different types of specimen, e.g. Saliva, Blood, Pus, etc.");
+		setGlobalProperty(
+		    administrationService,
+		    SPECIMEN_SITE_CONCEPT_UUID,
+		    "159959AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+		    "The UUID of a concept representing a group or set of anatomical source site from where the specimen is obtained, e.g. Bone, Tissue, etc.");
+		setGlobalProperty(administrationService, TEST_UNITS_CONCEPT_UUID, "162384AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+		    "The UUID of a concept representing a group or set of various measurement units (also used to measure dosage quantity).");
+		setGlobalProperty(administrationService, UPLOAD_FILE_EXTENSIONS, FILE_EXTENSIONS_NAMES, "");
+		setGlobalProperty(administrationService, LAB_ORDER_TYPE_UUID, "33ccfcc6-0370-102d-b0e3-001ec94a0cc1",
+		    "The UUID of the Order type representing a Lab Test Order.");
 	}
 }
